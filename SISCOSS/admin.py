447aa16b9d17b80d988e6a9aef880af300813969
CLLKoZ <npleitez29@gmail.com>
2021-06-22 11:37:33 -0600

    Agregando formularios
    Hago una pequeña pausa

--- SISCOSS/admin.py
+++ SISCOSS/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from SISCOSS.models import Maestro, Facultad, Escuela, Carrera
+from SISCOSS.models import Maestro, Facultad, Escuela, Carrera, EstadoSolicitud, TipoServicio
 # Register your models here.
 
 admin.site.register(Maestro)
 admin.site.register(Facultad)
 admin.site.register(Escuela)
 admin.site.register(Carrera)
+admin.site.register(EstadoSolicitud)
+admin.site.register(TipoServicio)
--- SISCOSS/forms.py
+++ SISCOSS/forms.py
@@ -1,1 +1,42 @@
 from django import forms
+
+from SISCOSS.models import Institucion, Facultad, Carrera, Solicitud, TipoServicio
+
+class InstitucionForm(forms.ModelForm):
+	class Meta:
+		model = Institucion
+
+		fields = [
+			'nombre_ins',
+			'email_ins',
+		]
+
+		labels = {
+			'nombre_ins': 'Nombre de la Institucion: ',
+			'email_ins': 'Email de la Institucion: ',
+		}
+
+		widgets = {
+			'nombre_ins': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingrese el nombre de la institucion'}),
+			'email_ins': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Ingrese el email de la institucion'}),
+		}
+
+class SolicitudForm(forms.ModelForm):
+	class Meta:
+		model = Solicitud
+
+		fields = [
+			'facultad_soli',
+			'carrera_soli',
+			'tipo_servi_soli',
+			'estado_soli',
+			'comentario',
+		]
+
+		labels = {
+			
+		}
+
+		widgets = {
+			
+		}
