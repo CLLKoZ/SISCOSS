2d435ce82248ed8f10ba7a52dbcb08b2ff5b3998
RicardoZ7 <75191449+RicardoZ7@users.noreply.github.com>
2021-06-23 00:34:06 -0600

    Modificaciones estado de solicitud y asignar encargado

--- SISCOSS/urls.py
+++ SISCOSS/urls.py
@@ -4,8 +4,8 @@ from SISCOSS import views
 urlpatterns = [
     path('', views.index, name="Inicio"),
     path('ver_estado_solicitud/', views.ver_estado_solicitud.as_view(), name="EstadoSolicitud"),
-    path('asignar_encargado_escuela/', views.asignar_encargado_escuela, name="AsignarEncargadoEscuela"),
+    path('asignar_encargado_escuela/', views.asignar_encargado_escuela.as_view(), name="AsignarEncargadoEscuela"),
     path('solicitud/', views.SolicitudCrear.as_view(), name = "Solicitud"),
-    path('ver_solicitudes_recibidas', views.ver_estado_solicitud.as_view() name="SolicitudesRecibidas"),
-    path('evaluar_solicitud', name="Evaluar")
+    path('ver_solicitudes_recibidas/', views.ver_solicitudes_recibidas.as_view(), name="SolicitudesRecibidas"),
+    #path('evaluar_solicitud/', name="Evaluar"),
 ]
--- SISCOSS/views.py
+++ SISCOSS/views.py
@@ -2,21 +2,20 @@ from django.shortcuts import render, HttpResponse
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView
 from SISCOSS.forms import InstitucionForm, SolicitudForm
-from SISCOSS.models import Solicitud
+from SISCOSS.models import Solicitud, Escuela
 
 # Create your views here.
 
 def index(request):
 	return render(request, "SISCOSS/index.html")
 
-
 class ver_estado_solicitud(ListView):
 	model = Solicitud
 	template_name= 'SISCOSS/ver_estado_solicitud.html'
 	
-
-def asignar_encargado_escuela(request):
-	return render(request, "SISCOSS/asignar_encargado_escuela.html")
+class asignar_encargado_escuela(ListView):
+	model = Escuela
+	template_name= 'SISCOSS/asignar_encargado_escuela.html'
 
 class SolicitudCrear(CreateView):
 	model = Solicitud
