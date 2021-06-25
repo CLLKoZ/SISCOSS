1e0e0f32af46533daa3cd799af0f22d4a09bf276
CLLKoZ <npleitez29@gmail.com>
2021-06-24 19:37:49 -0600

    Para host

--- Proyecto_DSI/settings.py
+++ Proyecto_DSI/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a$69^zg^x30-#5uh+jo569hya*%$xp17373*@zr5u_!iv!@sf('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -124,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
