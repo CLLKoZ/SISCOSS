287a8e8f0fd0a4d12e9a642869ec1ee931514ab9
CLLKoZ <npleitez29@gmail.com>
2021-06-24 19:07:48 -0600

    setting for host

--- Proyecto_DSI/settings.py
+++ Proyecto_DSI/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a$69^zg^x30-#5uh+jo569hya*%$xp17373*@zr5u_!iv!@sf('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
